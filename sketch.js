749f76ba7e0708e0ababfe1fc429b85a6745f2eb
NyaNyong <sook9232@naver.com>
2023-12-03 17:44:06 +0900

    particleamount 추가

--- sketch.js
+++ sketch.js
@@ -8,13 +8,15 @@ let att;
 let sec = 0;
 
 let powerslider;
-let repelamountslider;
 
 let gui;
 let params = {
   repelleramount : 0,
   repelleramountMin : 0,
   repelleramountMax : 5,
+  particleamount : 3,
+  particleamountMin : 0,
+  particleamountMax : 10,
 }
 
 
@@ -27,8 +29,6 @@ function setup() {
   att = new Attractor(20, height/2);
   powerslider = createSlider(200, 400, 200);
   powerslider.position(20,20);
-  repelamountslider = createSlider(0,10,0,1);
-  repelamountslider.position(20, 500);
   
   gui = createGui('test slider');
   gui.addObject(params);
@@ -41,19 +41,15 @@ function draw() {
   
   
   let repelpower = powerslider.value();
-    
-  repeller.setPower(repelpower);
-  repeller.move(createVector(mouseX,mouseY));
   
   if (sec === 50){
     att.move(createVector(random(0,400),random(0,400)));
     sec = 0;
   }
   
-  
-  emitter.addParticle();
-  emitter.addParticle();
-  emitter.addParticle();
+  for (let j=0; j < params.particleamount; j++){
+    emitter.addParticle();
+  }
   
   
   let gravity = createVector(0, 0.1);
@@ -63,7 +59,6 @@ function draw() {
   emitter.applyAttractor(att);
   emitter.run();
 
-  repeller.show();
   att.show();
 
   for (let i=0; i < params.repelleramount; i++){
